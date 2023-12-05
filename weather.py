4bf95f35e5b3d3edb279958bfc13223c0c1258de
DmitryKosmos <152957455+DmitryKosmos@users.noreply.github.com>
2023-12-05 10:46:48 +0300

    Create weather.py

---
+++ weather.py
@@ -0,0 +1,25 @@
+# подключаем библиотеку для работы с запросами
+import requests
+# указываем город
+city = 'Москва'
+# формируем запрос
+url = 'https://api.openweathermap.org/data/2.5/weather?q='+city+'&units=metric&lang=ru&appid=3f472b11c2822d4d48af1c44a7ad2d5e'
+# отправляем запрос на сервер и сразу получаем результат
+weather_data = requests.get(url).json()
+# получаем данные о температуре и о том, как она ощущается
+temperature = round(weather_data['main']['temp'])
+temperature_min = round(weather_data['main']['temp_min'])
+temperature_max = round(weather_data['main']['temp_max'])
+temperature_sr = round((temperature_min + temperature_max)/2)
+temperature_feels = round(weather_data['main']['feels_like'])
+sky = weather_data['weather'][0]['description']
+
+if __name__ == "__main__":
+    print('Сейчас в городе', city, str(temperature), '°C')
+    print('Ощущается как', str(temperature_feels), '°C')
+    print("min", temperature_min)
+    print("max", temperature_max)
+    print("sred", temperature_sr)
+    print(sky)
+    print(weather_data) ##на всякий случай
+
