dc8840073860f61626f89aef5a5d76f22e1e17ba
DmitryKosmos <152957455+DmitryKosmos@users.noreply.github.com>
2023-12-05 10:45:08 +0300

    main.py

---
+++ main.py
@@ -0,0 +1,26 @@
+##__temp_comf = 25 ## число, которое должно получиться в сумме погоды и одежды
+
+import  weather
+import  clothe
+import random
+import messages
+
+if weather.temperature_feels >= 20:
+    print(random.choice(messages.hot))
+else:
+    if weather.temperature_feels >= 15:
+        print(random.choice(messages.warm))
+    else:
+        if weather.temperature_feels >= 7:
+            print(random.choice(messages.mid))
+        else:
+            if weather.temperature_feels >= -20:
+                print(random.choice(messages.cold))
+            else:
+                 print(random.choice(messages.dubak))
+
+print('Сейчас в городе', weather.city, str(weather.temperature), '°C')
+print('Ощущается как', str(weather.temperature_feels), '°C')
+print("min", weather.temperature_min)
+print("max", weather.temperature_max)
+print(weather.sky)
